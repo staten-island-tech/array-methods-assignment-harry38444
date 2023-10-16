b155b1e9643e7d6e287ede990099cca84f4f7862
harry38444 <harry38444@gmail.com>
2023-10-16 10:09:17 -0400

    a
    a

--- app.js
+++ app.js
@@ -5,30 +5,26 @@ const students = [{
     intelligent: true,
     DOB: 2007,
     siblings: ["JJ", "JH"],
-    },{
+},{
 firstName: "rick",
     lastName:"Li",
-    graduating: false,
-    intelligent: false,
+    graduating: true,
+    intelligent: true,
     DOB: 2008,
     siblings: ["rat", "ratty"],
-
-    },{
+},{
     
     firstName: "ty",
     lastName:"yap",
     graduating: false,
     intelligent: false,
     DOB: 2008,
-    siblings: ["tapper", "yapper"],
-    },{
+    siblings: false,
 }
 ]
-
-
-const siblings = students.forEach((student)=> student.siblings.forEach((siblings) =>console.log(siblings)));
-students.forEach((student)=> console.log(student.firstName))
 const graduating = students.filter((student)=> student.graduating === true)
 console.log(graduating)
-const Has_siblings = students.filter((student)=> student.intelligent !== false)
-console.log(Has_siblings)
+const has_siblings = students.filter((student)=> student.siblings !== false)
+console.log(has_siblings)
+const siblings = students.forEach((student)=> student.siblings.forEach((siblings) =>console.log(siblings)))
+students.forEach((student)=> console.log(student.firstName))
